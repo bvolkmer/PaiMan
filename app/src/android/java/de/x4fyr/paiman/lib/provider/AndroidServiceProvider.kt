a59ba5d711408849678dfad8901bd819b728288f
Benedikt Volkmer <benedikt_v@web.de>
2017-04-19 18:41:02 +0200

    Cleanup and upstream snapshot tornadofx dependencies

--- app/src/android/java/de/x4fyr/paiman/lib/provider/AndroidServiceProvider.kt
+++ app/src/android/java/de/x4fyr/paiman/lib/provider/AndroidServiceProvider.kt
@@ -1,12 +1,9 @@
 package de.x4fyr.paiman.lib.provider
 
-import de.x4fyr.paiman.lib.provider.ServiceProvider
 import javafxports.android.FXActivity
-import com.couchbase.lite.android.AndroidContext
 
 /**
  * @author x4fyr
  * Created on 3/17/17.
  */
-class AndroidServiceProvider : ServiceProvider(AndroidContext(FXActivity.getInstance())) {
-}
+class AndroidServiceProvider : ServiceProvider(AndroidContext(FXActivity.getInstance()))
