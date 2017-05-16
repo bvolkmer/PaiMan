776a9231bba77441bd3c2895b5ea3de9bdab33e8
Benedikt Volkmer <benedikt_v@web.de>
2017-05-16 20:21:08 +0200

    Remove javafx-ports and add new android module

---
+++ android/src/main/java/de/x4fyr/paiman/MainActivity.java
@@ -0,0 +1,13 @@
+package de.x4fyr.paiman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
