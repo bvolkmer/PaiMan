1804db134e8b56ea462391368322a27bc4635112
Benedikt Volkmer <bvolkmer@techfak.uni-bielefeld.de>
2017-08-14 20:56:52 +0200

    android: created add painting dialog

--- android/src/main/java/de/x4fyr/paiman/MainActivity.kt
+++ android/src/main/java/de/x4fyr/paiman/MainActivity.kt
@@ -1,13 +1,20 @@
 package de.x4fyr.paiman
 
 import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.View.INVISIBLE
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.util.TypedValue
+import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import de.x4fyr.paiman.app.ApplyingDialogFragment
 import de.x4fyr.paiman.lib.provider.AndroidPictureProvider
 import de.x4fyr.paiman.lib.provider.AndroidServiceProvider
@@ -16,10 +23,13 @@ import de.x4fyr.paiman.lib.services.PaintingService
 import de.x4fyr.paiman.lib.services.QueryService
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import org.jetbrains.anko.coroutines.experimental.bg
-import org.jetbrains.anko.doAsync
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.displayMetrics
+import org.jetbrains.anko.image
 import org.jetbrains.anko.support.v4.onRefresh
-import org.jetbrains.anko.uiThread
+import java.io.InputStream
 
 /** Main Activity of the app, which is launched first and shows a list of current paintings
  *
@@ -34,8 +44,12 @@ class MainActivity: AppCompatActivity() {
     private lateinit var queryService: QueryService
     private lateinit var paintingService: PaintingService
     private lateinit var designService: DesignService
+
     private var onActivityResultHandler: ((Int, Int, Intent?) -> Unit)? = null
     private var activeDialogFragment: ApplyingDialogFragment? = null
+
+    private lateinit var gridAdapter: GridAdapter
+    private var models: MutableList<Model> = mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +61,14 @@ class MainActivity: AppCompatActivity() {
         queryService = serviceProvider.queryService
         paintingService = serviceProvider.paintingService
         designService = serviceProvider.designService
+        gridAdapter = GridAdapter(models, resources)
+
+        val spanCount = Math.floor(
+                displayMetrics.widthPixels/TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 106.toFloat(),
+                        displayMetrics).toDouble()).toInt()
+        paintingGrid.layoutManager = StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.VERTICAL)
+        paintingGrid.adapter = gridAdapter
+
         fab.setOnClickListener { _ ->
             val fragment: DialogFragment = AddPaintingFragment()
             // if (designService.isLargeDevice) {
@@ -60,36 +82,46 @@ class MainActivity: AppCompatActivity() {
                 }
             }*/
         }
-        progressBar.apply {
-            progress = 0
-            secondaryProgress = 0
-            max = 10
+        // Register query listener
+        queryService.allPaintingsQuery.toLiveQuery().apply {
+            addChangeListener {
+                async(UI) {
+                    swipeRefreshLayout.isRefreshing = true
+                    val newModels = async(CommonPool) {
+                        if (it.rows != null) {
+                            paintingService.getFromQueryResult(it.rows!!).map {
+                                Model(id = it.id, mainImage = paintingService.getPictureStream(it.mainPicture, it),
+                                        title = it.title, mainImageId = it.mainPicture.id)
+                            }
+                        } else listOf<Model>()
+                    }
+                    models.clear()
+                    models.addAll(newModels.await())
+                    gridAdapter.notifyDataSetChanged()
+                    swipeRefreshLayout.isRefreshing = false
+                }
+            }
+            start()
         }
-        loadContent({ progressBar.progress = it }, { progressBar.visibility = INVISIBLE })
-        swipeRefreshLayout.onRefresh {
-            loadContent(completionHandler = { swipeRefreshLayout.isRefreshing = false })
-        }
+        swipeRefreshLayout.onRefresh { loadContent(completionHandler = { swipeRefreshLayout.isRefreshing = false }) }
     }
 
 
     /**
      *
      */
-    private fun loadContent(onProgress: (percent: Int) -> Unit = {}, completionHandler: () -> Unit = {}) {
-        doAsync {
-            bg {
-                queryService.allPaintingsQuery.toLiveQuery().apply {
-                    addChangeListener {
-                        paintingService.getFromQueryResult(it.rows)
-                    }
-                    start()
-                }
-                uiThread { onProgress(50) }
-            }.invokeOnCompletion {
-                uiThread {
-                    completionHandler()
+    private fun loadContent(completionHandler: () -> Unit = {}) {
+        async(UI) {
+            val newModels = async(CommonPool) {
+                paintingService.getFromQueryResult(queryService.allPaintingsQuery.run()).map {
+                    Model(id = it.id, mainImage = paintingService.getPictureStream(it.mainPicture, it),
+                            title = it.title, mainImageId = it.mainPicture.id)
                 }
             }
+            models.clear()
+            models.addAll(newModels.await())
+            gridAdapter.notifyDataSetChanged()
+            completionHandler()
         }
     }
 
@@ -122,6 +154,41 @@ class MainActivity: AppCompatActivity() {
     /** Button action on the current active dialog fragment */
     fun onDialogButton(view: View) {
         activeDialogFragment?.onDialogButton(view)
+    }
+
+    private data class Model(val id: String, val mainImage: InputStream, val title: String, val mainImageId: String)
+
+    private class GridAdapter(val models: MutableList<Model>, val resources: Resources)
+        : RecyclerView.Adapter<GridAdapter.ModelViewHolder>() {
+
+        /** See [RecyclerView.Adapter] */
+        override fun onBindViewHolder(holder: ModelViewHolder, position: Int) {
+            val model = models[position]
+            async(UI) {
+                val image: BitmapDrawable = async(CommonPool) {
+                    BitmapDrawable(resources,
+                            Bitmap.createScaledBitmap(BitmapFactory.decodeStream(model.mainImage), 100, 100, false))
+                }.await()
+                holder.mainImage.image = image
+                holder.title.text = model.title
+            }
+        }
+
+        /** See [RecyclerView.Adapter] */
+        override fun getItemCount(): Int = models.size
+
+        /** See [RecyclerView.Adapter] */
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder
+                = ModelViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_main, parent, false))
+
+        private class ModelViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            /** [CardView] container */
+            var container: CardView = itemView.findViewById<CardView>(R.id.painting_preview_card)
+            /** [ImageView] containing the main image preview */
+            var mainImage: ImageView = itemView.findViewById<ImageView>(R.id.painting_preview_image)
+            /** [TextView] containing the title */
+            var title: TextView = itemView.findViewById<TextView>(R.id.painting_preview_title)
+        }
     }
 
 }
--- libpaiman/src/main/kotlin/de/x4fyr/paiman/lib/services/MainServiceImpl.kt
+++ libpaiman/src/main/kotlin/de/x4fyr/paiman/lib/services/MainServiceImpl.kt
@@ -5,8 +5,9 @@ import de.x4fyr.paiman.lib.adapter.database.PaintingCRUDAdapter
 import de.x4fyr.paiman.lib.adapter.database.PictureCRUDAdapter
 import de.x4fyr.paiman.lib.adapter.database.QueryAdapter
 import de.x4fyr.paiman.lib.domain.*
-import java.io.InputStream
 import org.threeten.bp.LocalDate
+import java.io.InputStream
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import java.util.logging.Logger
 
 /**
@@ -16,18 +17,31 @@ import java.util.logging.Logger
  */
 internal class MainServiceImpl(private var paintingCRUDAdapter: PaintingCRUDAdapter,
                                queryAdapter: QueryAdapter,
-                               private val pictureCRUDAdapter: PictureCRUDAdapter) :
+                               private val pictureCRUDAdapter: PictureCRUDAdapter):
         PaintingService,
         QueryService by queryAdapter {
 
-    override fun get(id: String): SavedPainting = paintingCRUDAdapter.read(id) ?: throw ServiceException(
-            "Could not get")
-
-    override fun getAll(ids: Set<String>): Set<SavedPainting> = paintingCRUDAdapter.read(ids)
+    private val lock: ReentrantReadWriteLock = ReentrantReadWriteLock()
+    private val readLock = lock.readLock()
+    private val writeLock = lock.writeLock()
 
     private var LOG = Logger.getLogger(this::class.simpleName)
 
     private val dummyPicture = Picture("dummy")
+
+    override fun get(id: String): SavedPainting {
+        readLock.lock()
+        val result = paintingCRUDAdapter.read(id)
+        readLock.unlock()
+        return result ?: throw ServiceException("Could not get")
+    }
+
+    override fun getAll(ids: Set<String>): Set<SavedPainting> {
+        readLock.lock()
+        val result = paintingCRUDAdapter.read(ids)
+        readLock.unlock()
+        return result
+    }
 
     override fun composeNewPainting(title: String,
                                     mainPicture: InputStream,
@@ -41,6 +55,7 @@ internal class MainServiceImpl(private var paintingCRUDAdapter: PaintingCRUDAdap
                 references = setOf(),
                 sellingInfo = sellingInfo,
                 tags = tags, mainPicture = dummyPicture)
+        writeLock.lock()
         var savedPainting = paintingCRUDAdapter.create(newPainting) ?: throw ServiceException("Could not create " +
                 "painting")
         LOG.info("Saving mainPicture to DB")
@@ -55,78 +70,115 @@ internal class MainServiceImpl(private var paintingCRUDAdapter: PaintingCRUDAdap
                 wip = wipIDs.map(::Picture).toSet(),
                 references = refIDs.map(::Picture).toSet()
         )) ?: throw ServiceException("Could not add pictures to painting")
+        writeLock.unlock()
         return savedPainting
     }
 
     override fun replaceMainPicture(painting: SavedPainting,
                                     newPicture: InputStream,
                                     moveOldToWip: Boolean): SavedPainting {
+        writeLock.lock()
         val mainPictureID = pictureCRUDAdapter.createPicture(newPicture, paintingID = painting.id)
-        return paintingCRUDAdapter.update(
-                painting = painting.copy(
-                        mainPicture = Picture(mainPictureID),
-                        wip = if (moveOldToWip) painting.wip + painting.mainPicture else painting.wip))
-                ?: throw ServiceException("Could not replace main picture")
+        val result = paintingCRUDAdapter.update(painting = painting.copy(
+                mainPicture = Picture(mainPictureID),
+                wip = if (moveOldToWip) painting.wip + painting.mainPicture else painting.wip))
+        writeLock.unlock()
+        return result ?: throw ServiceException("Could not replace main picture")
     }
 
-    override fun changePainting(painting: SavedPainting): SavedPainting = paintingCRUDAdapter.update(painting)
-            ?: throw ServiceException("Could not update painting")
+    override fun changePainting(painting: SavedPainting): SavedPainting {
+        writeLock.lock()
+        val result = paintingCRUDAdapter.update(painting)
+        writeLock.unlock()
+        return result ?: throw ServiceException("Could not update painting")
+    }
 
     override fun sellPainting(painting: SavedPainting,
                               purchaser: Purchaser,
                               date: LocalDate,
-                              price: Double): SavedPainting = paintingCRUDAdapter.update(painting.copy(
-            sellingInfo = SellingInformation(purchaser = purchaser, date = date, price = price)))
-            ?: throw ServiceException("Could not sell painting")
+                              price: Double): SavedPainting {
+        writeLock.lock()
+        val result = paintingCRUDAdapter.update(
+                painting.copy(sellingInfo = SellingInformation(purchaser = purchaser, date = date, price = price)))
+        writeLock.unlock()
+        return result ?: throw ServiceException("Could not sell painting")
+
+    }
 
     override fun addWipPicture(painting: SavedPainting, images: Set<InputStream>): SavedPainting {
+        writeLock.lock()
         val wipIDs = images.map { pictureCRUDAdapter.createPicture(pictureStream = it, paintingID = painting.id) }
-        return paintingCRUDAdapter.update(painting.copy(wip = painting.wip + wipIDs.map(::Picture)))
-                ?: throw ServiceException("Could not add wip")
+        val result = paintingCRUDAdapter.update(painting.copy(wip = painting.wip + wipIDs.map(::Picture)))
+        writeLock.unlock()
+        return result ?: throw ServiceException("Could not add wip")
     }
 
     override fun removeWipPicture(painting: SavedPainting, images: Set<String>): SavedPainting {
+        writeLock.lock()
         val savedPainting = paintingCRUDAdapter.update(painting.copy(wip = painting.wip - images.map(::Picture)))
         images.forEach { pictureCRUDAdapter.deletePicture(id = it, paintingID = painting.id) }
+        writeLock.unlock()
         return savedPainting ?: throw ServiceException("Could not remove wip")
     }
 
     override fun addReferences(painting: SavedPainting,
                                references: Set<InputStream>): SavedPainting {
+        writeLock.lock()
         val refIDs = references.map { pictureCRUDAdapter.createPicture(pictureStream = it, paintingID = painting.id) }
-        return paintingCRUDAdapter.update(painting.copy(references = painting.references + refIDs.map(::Picture)))
-                ?: throw ServiceException("Could not add reference")
+        val result = paintingCRUDAdapter.update(painting.copy(references = painting.references + refIDs.map(::Picture)))
+        writeLock.unlock()
+        return result ?: throw ServiceException("Could not add reference")
     }
 
     override fun removeReferences(painting: SavedPainting, references: Set<String>): SavedPainting {
+        writeLock.lock()
         val savedPainting = paintingCRUDAdapter.update(
                 painting.copy(references = painting.references - references.map(::Picture)))
         references.forEach { pictureCRUDAdapter.deletePicture(id = it, paintingID = painting.id) }
+        writeLock.unlock()
         return savedPainting ?: throw ServiceException("Could not remove painting")
     }
 
-    override fun addTags(painting: SavedPainting, tags: Set<String>): SavedPainting = paintingCRUDAdapter.update(
-            painting.copy(tags = painting.tags + tags)) ?: throw ServiceException("Could not add tags")
+    override fun addTags(painting: SavedPainting, tags: Set<String>): SavedPainting {
+        writeLock.lock()
+        val result = paintingCRUDAdapter.update(painting.copy(tags = painting.tags + tags))
+        writeLock.unlock()
+        return result ?: throw ServiceException("Could not add tags")
+    }
 
-    override fun removeTags(painting: SavedPainting, tags: Set<String>): SavedPainting = paintingCRUDAdapter.update(
-            painting.copy(tags = painting.tags - tags)) ?: throw ServiceException("Could not remove tags")
+    override fun removeTags(painting: SavedPainting, tags: Set<String>): SavedPainting {
+        writeLock.lock()
+        val result = paintingCRUDAdapter.update(painting.copy(tags = painting.tags - tags))
+        writeLock.unlock()
+        return result ?: throw ServiceException("Could not remove tags")
+    }
 
-    override fun getPictureStream(picture: Picture,
-                                  painting: SavedPainting): InputStream = pictureCRUDAdapter.readPicture(id =
-    picture.id, paintingID = painting.id) ?: throw ServiceException("Could not get picture")
+    override fun getPictureStream(picture: Picture, painting: SavedPainting): InputStream {
+        readLock.lock()
+        val result = pictureCRUDAdapter.readPicture(id = picture.id, paintingID = painting.id)
+        readLock.unlock()
+        return result ?: throw ServiceException("Could not get picture")
+    }
 
-    override fun getAllPictureStreams(pictures: Set<Picture>,
-                                      painting: SavedPainting): Set<InputStream> = pictureCRUDAdapter
-            .readPictures(pictures.map { it.id }.toSet(), painting.id)
+    override fun getAllPictureStreams(pictures: Set<Picture>, painting: SavedPainting): Set<InputStream> {
+        readLock.lock()
+        val result = pictureCRUDAdapter.readPictures(pictures.map { it.id }.toSet(), painting.id)
+        readLock.lock()
+        return result
+    }
 
     override fun getFromQueryResult(queryEnumerator: QueryEnumerator): Set<SavedPainting> = getAll(
             queryEnumerator.map { it.key.toString() }.toSet())
 
     override fun delete(painting: SavedPainting) {
+        writeLock.lock()
         paintingCRUDAdapter.delete(id = painting.id)
+        writeLock.unlock()
     }
 
     override fun delete(paintingId: String) {
+        writeLock.lock()
         paintingCRUDAdapter.delete(id = paintingId)
+        writeLock.unlock()
     }
 }
