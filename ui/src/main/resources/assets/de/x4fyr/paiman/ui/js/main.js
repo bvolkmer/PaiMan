8fd50bb5a71f49c789a8c922a486d4813b0fbc34
Benedikt Volkmer <bvolkmer@techfak.uni-bielefeld.de>
2018-10-09 14:00:48 +0200

    ui: fix reloading on overview

--- ui/src/main/resources/assets/de/x4fyr/paiman/ui/js/main.js
+++ ui/src/main/resources/assets/de/x4fyr/paiman/ui/js/main.js
@@ -2,3 +2,11 @@
 function showError(message) {
     ons.notification.toast(message, {timeout: 3000});
 }
+
+function clearChildren(node) {
+    while (node.hasChildNodes()) {
+        var firstChild = node.childNodes[0];
+        clearChildren(firstChild);
+        node.removeChild(firstChild)
+    }
+}
